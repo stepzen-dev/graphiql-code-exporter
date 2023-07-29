0a16e74feb211cd10f1eaa12de63fb368095647a
Michael Trythall <m@mtrythall.com>
2023-07-29 15:52:05 -0400

    Fix connect icon

--- src/useExporterPlugin.tsx
+++ src/useExporterPlugin.tsx
@@ -2,6 +2,8 @@ import {useMemo, useRef} from 'react'
 import type {ComponentType} from 'react'
 import CodeExporter from './CodeExporter'
 
+import ConnectIcon from './icons/connect.svg'
+
 // Copied from `@graphiql/react` to avoid adding a dependency for a single type definition
 type GraphiQLPlugin = {
   /**
@@ -27,19 +29,7 @@ function useExporterPlugin(props: any) {
     () => ({
       title: 'Connect',
       icon: () => (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15"
-          />
-        </svg>
+        <ConnectIcon />
       ),
       content: () => (
         <CodeExporter codeMirrorTheme="graphiql" {...propsRef.current} />
